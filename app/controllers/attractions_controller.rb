048af35a412f84ebabe1be6091b6df33cbf278e1
Kevin Huang <yellow.khaos@gmail.com>
2018-07-23 05:31:22 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,5 @@
 class AttractionsController <ApplicationController
-  before_action :
+  before_action :find_attraction, only: [:show, :edit, :update]
 
   def index
     @attractions = Attraction.all
@@ -22,7 +22,7 @@ class AttractionsController <ApplicationController
   end
 
   def new
-    
+
   end
 
   def create
