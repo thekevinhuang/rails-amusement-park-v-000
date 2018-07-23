12769c09d499258a27d64e5e360e500a6040cb42
Kevin Huang <yellow.khaos@gmail.com>
2018-07-23 02:54:59 +0000

    automatically backed up by learn

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,20 @@
+class AttractionsController <ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+
+  def ride
+    ride = Ride.new
+
+    ride.user = current_user
+    ride.attraction = Attraction.find_by(id: params[:attraction_id])
+    ride.save
+    flash[:message] = ride.take_ride
+    redirect_to user_path(current_user)
+  end
+end
